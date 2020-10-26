ffbcc337b736370dc06a9a8a24f8a9b48f33d943
viralsir <viralsir2018@gmail.com>
2020-10-26 12:43:47 +0530

    task app updated with submit data and session functionality.

--- siddhart_django/task/views.py
+++ siddhart_django/task/views.py
@@ -1,21 +1,34 @@
 from django import forms
-from django.shortcuts import render
-
-tasks=["check email","paid bill","check bank balance"]
+from django.shortcuts import render,reverse
+from django.http import HttpResponseRedirect
 
 
 class NewTaskForm(forms.Form):
     task=forms.CharField(label="new Task ")
-    priority=forms.IntegerField(label="Priority :",min_value=2,max_value=10)
+    priority=forms.IntegerField(label="Priority :",min_value=1,max_value=3)
     
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+
     return render(request,"task/index.html",{
-        'tasks':tasks
+        'tasks':request.session["tasks"]
     })
 
 def add(request):
+    if request.method=='POST':
+        form=NewTaskForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("index"))
+        else :
+            return render(request, "task/add.html", {
+                "form": form
+            })
+
     return render(request,"task/add.html",{
         "form":NewTaskForm()
     })
